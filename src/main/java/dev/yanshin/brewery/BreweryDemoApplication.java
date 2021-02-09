079379bae724d69f63e399c6b0e14a9576d289e7
starchie-d <dmitry.ianshin@gmail.com>
2021-02-09 11:02:33 +0300

    Initialize by Spring Initializr

---
+++ src/main/java/dev/yanshin/brewery/BreweryDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.yanshin.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryDemoApplication.class, args);
+	}
+
+}
